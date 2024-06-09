9885d9f06a5f940b60df067dbe982aafc0d8fb82
DarkSkyFullOFStars <34119991+darkskyfullofstars@users.noreply.github.com>
2024-06-09 02:28:13 -0500

    removed broken client

--- MapleStory v203.4 Full Client/nxsteam/src/main/session.js
+++
@@ -1,6 +0,0 @@
-const { app, session } = require('electron')
-
-session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders['User-Agent'] = `steam-connector/${app.getVersion()}, electron/${process.versions.electron}`;
-    callback({ cancel: false, requestHeaders: details.requestHeaders });
-  });
